01d165cf399ef5de8014c7acfcd751571779ec81
vaibhavkumar <kumarvaibhav8735@gmail.com>
2025-04-13 02:09:57 +0530

    HIgher order function

--- pracctice.js
+++ pracctice.js
@@ -615,3 +615,28 @@
   // Step 3: Call greet and pass sayHi as the callback
 //   greet(sayHi);
   
+//Higher order function
+const radius =[3,1,2,4];
+
+const area = function (radius){
+  return Math.PI * radius;
+};
+
+const circumference = function (radius){
+  return 2 * Math.PI * radius;
+};
+
+const diameter = function(radius){
+  return 2 * radius;
+};
+
+const calculate = function(radius , logic){
+  const output = [];
+  for(let i = 0; i< radius.length; i++){
+    output.push(logic(radius[i]));
+  }
+  return output;
+}
+console.log(calculate(radius, area));
+console.log(calculate(radius, circumference));
+console.log(calculate(radius, diameter));
